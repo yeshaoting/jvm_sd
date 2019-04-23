dbf4100d634cc24c75b93a4a16b1480343eb4437
叶绍亭 <yeshaoting@bytedance.com>
2019-04-23 15:23:55 +0800

    all pageKey return stats data
    Change-Id: I18a93e4ade1e5260bafa01654874e92242582c74

--- src/main/java/cn/yeshaoting/sd/SimpleCounterLimiter.java
+++ src/main/java/cn/yeshaoting/sd/SimpleCounterLimiter.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public class SimpleCounterLimiter {
 
     private static final int MAX_COUNTER = 10;
-    private static AtomicReference<Pair<Long, Integer>> counter = new AtomicReference(Pair.of(0, 0));
+    private static AtomicReference<Pair<Long, Integer>> counter = new AtomicReference(Pair.of(0L, MAX_COUNTER + 1));
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         executeTask();
@@ -19,18 +19,16 @@ public class SimpleCounterLimiter {
 
     private static Triple<Boolean, Long, Integer> limit() {
         while (true) {
-            System.out.println("retry cas action");
             Pair<Long, Integer> pair = counter.get();
             long currentSeconds = System.currentTimeMillis() / 1000;
 
             Pair<Long, Integer> newPair;
-            if (pair.getKey() > currentSeconds) {
+            if (pair.getKey() < currentSeconds) {
                 newPair = Pair.of(currentSeconds, 1);
             } else {
                 newPair = Pair.of(pair.getKey(), pair.getValue() + 1);
             }
 
-            System.out.println("fail hold cas3");
             boolean flag = counter.compareAndSet(pair, newPair);
             if (flag == false) {
                 System.out.println("fail hold cas");
@@ -38,35 +36,34 @@ public class SimpleCounterLimiter {
             }
 
             if (newPair.getValue() > MAX_COUNTER) {
-                System.out.println("fail hold cas2");
-                return Triple.of(true, pair.getKey(), newPair.getValue());
+                return Triple.of(true, newPair.getKey(), newPair.getValue());
             }
 
-            System.out.println("fail hold cas1");
-            return Triple.of(false, pair.getKey(), newPair.getValue());
+            return Triple.of(false, newPair.getKey(), newPair.getValue());
         }
     }
 
     private static void executeTask() throws ExecutionException, InterruptedException {
         ExecutorService worker = Executors.newFixedThreadPool(2);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 300; i++) {
             final int v = i;
-            Future<String> future = worker.submit(new Callable<String>() {
+            worker.execute(new Runnable() {
                 @Override
-                public String call() throws Exception {
-                    Thread.sleep(100);
-
-                    System.out.println("limit to here12");
+                public void run() {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     Triple<Boolean, Long, Integer> triple = limit();
-                    System.out.println("limit to here1");
                     String feature = triple.getLeft() ? "blocked" : "passed";
                     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
                     String result = String.format("%s\t%s\t%s\t %s-%s-%s", sdf.format(new Date()), triple.getMiddle(), triple.getRight(), v, Thread.currentThread().getName(), feature);
                     System.out.println(result);
-                    return result;
                 }
             });
 
         }
+        worker.shutdown();
     }
 }
