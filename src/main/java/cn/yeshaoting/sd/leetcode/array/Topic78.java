8d4c089030ad19f39d0e6af332ac406ee5fadab6
叶绍亭 <yeshaoting@bytedance.com>
2019-08-19 19:08:44 +0800

    solution 2sum
    Change-Id: I5f06f3d9c1c398ae35aba675478c88fbfda98b68

--- src/main/java/cn/yeshaoting/sd/leetcode/array/Topic78.java
+++ src/main/java/cn/yeshaoting/sd/leetcode/array/Topic78.java
@@ -4,6 +4,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+//
+// 说明：解集不能包含重复的子集。
+//
+// 示例:
+//
+// 输入: nums = [1,2,3]
+//输出:
+//[
+//  [3],
+//  [1],
+//  [2],
+//  [1,2,3],
+//  [1,3],
+//  [2,3],
+//  [1,2],
+//  []
+//]
+//
+public class Topic78 extends BaseArray {
+
+    public static void main(String[] args) throws IOException {
+        int[] nums = stringToIntegerArray("[1,2,3]");
+        List<List<Integer>> ret = new Solution78_A().subsets(nums);
+        String out = int2dListToString(ret);
+        System.out.print(out);
+    }
+
+}
+
 class Solution78_A {
 
     // 迭代法
@@ -26,14 +57,4 @@ class Solution78_A {
     }
 }
 
-public class Topic78 extends BaseArray {
-
-    public static void main(String[] args) throws IOException {
-        int[] nums = stringToIntegerArray("[1,2,3]");
-        List<List<Integer>> ret = new Solution78_A().subsets(nums);
-        String out = int2dListToString(ret);
-        System.out.print(out);
-    }
-
-}
 
