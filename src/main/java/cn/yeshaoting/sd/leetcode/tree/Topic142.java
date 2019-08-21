42b9270cf8b6825e62a36a94d580a30a0b077e49
叶绍亭 <yeshaoting@bytedance.com>
2019-08-21 18:43:30 +0800

    solution cycle link
    Change-Id: I2ed90abba1eb2d48ce92e858070c13f60d01ea0b

--- src/main/java/cn/yeshaoting/sd/leetcode/tree/Topic142.java
+++ src/main/java/cn/yeshaoting/sd/leetcode/tree/Topic142.java
@@ -1,13 +1,15 @@
-//给定一个链表，判断链表中是否有环。 
+package cn.yeshaoting.sd.leetcode.tree;//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
 //
 // 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。 
+//
+// 说明：不允许修改给定的链表。 
 //
 // 
 //
 // 示例 1： 
 //
 // 输入：head = [3,2,0,-4], pos = 1
-//输出：true
+//输出：tail connects to node index 1
 //解释：链表中有一个环，其尾部连接到第二个节点。
 // 
 //
@@ -16,7 +18,7 @@
 // 示例 2： 
 //
 // 输入：head = [1,2], pos = 0
-//输出：true
+//输出：tail connects to node index 0
 //解释：链表中有一个环，其尾部连接到第一个节点。
 // 
 //
@@ -25,7 +27,7 @@
 // 示例 3： 
 //
 // 输入：head = [1], pos = -1
-//输出：false
+//输出：no cycle
 //解释：链表中没有环。
 // 
 //
@@ -34,8 +36,7 @@
 // 
 //
 // 进阶： 
-//
-// 你能用 O(1)（即，常量）内存解决此问题吗？ 
+//你是否可以不用额外空间解决此题？ 
 //
 
 import cn.yeshaoting.sd.leetcode.link.ListNode;
@@ -48,20 +49,20 @@ import java.util.Set;
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
-public class Topic141 {
+public class Topic142 {
 
     // 需要额外空间；依赖一个set空间做重复判断；由于val可能重复，因此set需要是node的内存地址。
-    public boolean hasCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
         if (head == null) {
-            return false;
+            return null;
         }
 
         int idx = 0;
@@ -69,13 +70,13 @@ public class Topic141 {
         ListNode current = head;
         while (current != null) {
             if (all.contains(current)) {
-                return true;
+                return current;
             }
 
             all.add(current);
             current = current.next;
         }
 
-        return false;
+        return null;
     }
 }
