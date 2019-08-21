1b4143c5ddc08d919767c1cd8aabbd96be029217
叶绍亭 <yeshaoting@bytedance.com>
2019-08-21 17:46:10 +0800

    solution tree max depth
    Change-Id: Ie88b607705aab677bf66b8f2627f537be636c79a

---
+++ src/main/java/cn/yeshaoting/sd/leetcode/tree/Topic104.java
@@ -0,0 +1,39 @@
+package cn.yeshaoting.sd.leetcode.tree;//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+//
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Topic104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = root.left == null ? 0 : maxDepth(root.left);
+        int rightDepth = root.right == null ? 0 : maxDepth(root.right);
+        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+    }
+}
---
+++ src/main/java/cn/yeshaoting/sd/leetcode/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package cn.yeshaoting.sd.leetcode.tree;
+
+/*
+    description:
+    author: yeshaoting
+    time: 2019-08-21 17:43
+*/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
+
+
