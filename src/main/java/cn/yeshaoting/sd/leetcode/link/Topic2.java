59ed211f8e9988e884b9c4e767486488602f0dbf
叶绍亭 <yeshaoting@bytedance.com>
2020-01-06 16:14:41 +0800

    83. 删除排序链表中的重复元素
    Change-Id: I5b28b69b6a3fa4bcbee42fde87dfe283cb21ca62

--- src/main/java/cn/yeshaoting/sd/leetcode/link/Topic2.java
+++ src/main/java/cn/yeshaoting/sd/leetcode/link/Topic2.java
@@ -39,67 +39,67 @@ class Topic2 extends BaseLink {
         for (Pair<String, String> pair : all) {
             ListNode l1 = stringToListNode(pair.getKey());
             ListNode l2 = stringToListNode(pair.getValue());
-            ListNode ret = new Solution2_B().addTwoNumbers(l1, l2);
+            ListNode ret = new Solution().addTwoNumbers(l1, l2);
             String out = listNodeToString(ret);
             System.out.printf("l1=%s\tl2=%s\tout=%s\n", pair.getKey(), pair.getValue(), out);
         }
     }
 
-}
+    static class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode head = l1;
+            ListNode pre = null;
+            int carry = 0;
+            while (l1 != null || l2 != null || carry != 0) {
+                // 整理node关系
+                ListNode l1Node = l1 == null ? new ListNode(0) : l1;
+                ListNode l2Node = l2 == null ? new ListNode(0) : l2;
+                if (l1 == null) {
+                    pre.next = l1Node;
+                }
 
-class Solution2_A {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = l1;
-        ListNode pre = null;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            // 整理node关系
-            ListNode l1Node = l1 == null ? new ListNode(0) : l1;
-            ListNode l2Node = l2 == null ? new ListNode(0) : l2;
-            if (l1 == null) {
-                pre.next = l1Node;
+                // 处理当前次数值变化
+                int sum = l1Node.val + l2Node.val + carry;
+                l1Node.val = sum % 10;
+                carry = sum / 10;
+
+                // 下次迭代控制
+                pre = l1Node;
+                l1 = l1Node.next;
+                l2 = l2Node.next;
             }
 
-            // 处理当前次数值变化
-            int sum = l1Node.val + l2Node.val + carry;
-            l1Node.val = sum % 10;
-            carry = sum / 10;
-
-            // 下次迭代控制
-            pre = l1Node;
-            l1 = l1Node.next;
-            l2 = l2Node.next;
+            return head;
         }
-
-        return head;
     }
-}
 
-// FIXME the solution
-class Solution2_B {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = l1;
-        ListNode pre = null;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            // 整理node关系
-            ListNode l1Node = l1 == null ? new ListNode(0) : l1;
-            ListNode l2Node = l2 == null ? new ListNode(0) : l2;
-            if (l1 == null) {
-                pre.next = l1Node;
+    // FIXME the solution
+    static class Solution2 {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode head = l1;
+            ListNode pre = null;
+            int carry = 0;
+            while (l1 != null || l2 != null || carry != 0) {
+                // 整理node关系
+                ListNode l1Node = l1 == null ? new ListNode(0) : l1;
+                ListNode l2Node = l2 == null ? new ListNode(0) : l2;
+                if (l1 == null) {
+                    pre.next = l1Node;
+                }
+
+                // 处理当前次数值变化
+                int sum = l1Node.val + l2Node.val + carry;
+                l1Node.val = sum % 10;
+                carry = sum / 10;
+
+                // 下次迭代控制
+                pre = l1Node;
+                l1 = l1Node.next;
+                l2 = l2Node.next;
             }
 
-            // 处理当前次数值变化
-            int sum = l1Node.val + l2Node.val + carry;
-            l1Node.val = sum % 10;
-            carry = sum / 10;
-
-            // 下次迭代控制
-            pre = l1Node;
-            l1 = l1Node.next;
-            l2 = l2Node.next;
+            return head;
         }
-
-        return head;
     }
+
 }
--- src/main/java/cn/yeshaoting/sd/leetcode/link/Topic83.java
+++ src/main/java/cn/yeshaoting/sd/leetcode/link/Topic83.java
@@ -14,6 +14,10 @@ package cn.yeshaoting.sd.leetcode.link;
 //输出: 1->2->3 
 // Related Topics 链表
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /*
     description:
     author: yeshaoting
@@ -21,26 +25,34 @@ package cn.yeshaoting.sd.leetcode.link;
 public class Topic83 {
 
     public static void main(String[] args) {
-        Solution83 solution = new Solution83();
+        List<String> all = Lists.newArrayList("[]", "[1,1,2]", "[1]", "[1,1]", "[1,2,2,3,4,4]", "[1,2,3]");
+        for (String value : all) {
+            ListNode head = BaseLink.stringToListNode(value);
+            ListNode result = new Solution().deleteDuplicates(head);
+            System.out.printf("result=%s\tvalue=%s\n", BaseLink.listNodeToString(result), value);
+        }
+    }
+
+    static class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            ListNode current = head;
+            ListNode next = current.next;
+            while (next != null) {
+                if (current.val == next.val) {
+                    current.next = next.next;
+                    next = next.next;
+                } else {
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
     }
 
 }
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
- */
-class Solution83 {
-    public ListNode deleteDuplicates(ListNode head) {
-        return null;
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
 
